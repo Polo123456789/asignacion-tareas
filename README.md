24a3dc1f9b621ffa78f4ebfa4c4215d18dd21144
Polo123456789 <pabsan02@gmail.com>
2024-02-28 18:12:09 -0600

    Licencia

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Asignacion de tareas
 
-Cada 2 meses me va a tocar crear un cronograma para repartir unas tareas de
-igual dificultad a un grupo.
+Una herramienta con el fin de automatizar la generacion de programas de Audio
+y Video para el salon del Reino.
 
-En vez de dedicar media hora a hacerlo manualmente cada vez, aqui queda este
-proyectito.
+Permite crear una lista de asignaciones, hermanos que puedan cumplirlas,
+y especificar que responsabilidades puede cumplir cada hermano.
 
+# Licencia
+
+```
+Derechos de Autor (C) 2023 Pablo Sanchez <pabsan02@gmail.com>
+
+Este programa es software libre: puede redistribuirlo y/o modificarlo
+bajo los términos de la Licencia Pública General de GNU publicada por
+la Free Software Foundation, ya sea la versión 3 de la Licencia o
+(a su elección) cualquier versión posterior.
+
+Este programa se distribuye con la esperanza de que sea útil,
+pero SIN NINGUNA GARANTÍA; ni siquiera la garantía implícita de
+COMERCIALIZACIÓN o IDONEIDAD PARA UN FIN PARTICULAR. Consulte la
+Licencia Pública General de GNU para más detalles.
+```
--- src/App.vue
+++ src/App.vue
@@ -258,6 +258,28 @@ const importConfig = () => {
             asistencia a JOSUE ORELLANA
         </p>
     </div>
+    <div class="dont-print">
+            <footer class="text-center mx-auto" style="max-width: 40rem;">
+                <details>
+                    <summary>
+                       Copyright (C) 2023 Pablo Sanchez
+                    </summary>
+                    <p>
+                        Este programa es software libre: puede redistribuirlo y/o modificarlo
+                        bajo los términos de la Licencia Pública General de GNU publicada por
+                        la Free Software Foundation, ya sea la versión 3 de la Licencia o
+                        (a su elección) cualquier versión posterior.
+                    </p>
+                    <p>
+                        Este programa se distribuye con la esperanza de que sea útil,
+                        pero SIN NINGUNA GARANTÍA; ni siquiera la garantía implícita de
+                        COMERCIALIZACIÓN o IDONEIDAD PARA UN FIN PARTICULAR. Consulte la
+                        Licencia Pública General de GNU para más detalles.
+                    </p>
+                </details>
+            </footer>
+        </div>
+    
 </template>
 
 <style scoped>
