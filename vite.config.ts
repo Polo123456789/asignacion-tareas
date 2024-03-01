783535990ab4c5213eb266c2055641cccab0a0e9
Polo123456789 <pabsan02@gmail.com>
2024-03-01 10:12:34 -0600

    (-) Fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://polo123456789.github.io/generador-programas-av/'
 })
